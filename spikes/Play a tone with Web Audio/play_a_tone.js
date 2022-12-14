9680296c18bc6c79947fb12dffe83897d7b57d02
Tom Barnsbury <ruralmyth@gmail.com>
2022-12-14 19:57:47 +0000

    Update 'Play a tone' spike to use a button to trigger playing the tone

--- spikes/Play a tone with Web Audio/play_a_tone.js
+++ spikes/Play a tone with Web Audio/play_a_tone.js
@@ -1,15 +1,11 @@
-console.log("Play a tone!");
-
 const toneContext = new AudioContext();
 
-// function tone_oscillator() {
-
+function tone_oscillator() {
   const oscillator = toneContext.createOscillator();
-
   oscillator.connect(toneContext.destination);
-
   oscillator.start();
-
   oscillator.stop(toneContext.currentTime + 2);
+}
 
-// }
+const playButton = document.querySelector("#play-a-tone");
+playButton.addEventListener('click', tone_oscillator);
